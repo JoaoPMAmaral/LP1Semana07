f82c4f9e4b867fe02bb445494e05493cb0b64459
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-04-23 15:42:03 +0100

    created powercell and playerstats

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,17 @@
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames;
+
+        public float HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+        }
+    }
+}
